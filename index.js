96d9c72d1d02425bfaabcea49ccdb298af61b642
n <n>
2025-02-05 16:51:49 -0600

    Step 1, created basic site and added database

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const { userRouter } = require('./routes/user');
+const { courseRouter } = require('./routes/course');
+const { adminRouter } = require('./routes/admin');
+
+const app = express();
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/admin", adminRouter);
+app.use("/api/v1/course", courseRouter);
+
+async function main() {
+    await mongoose.connect("mongodb+srv://aryanpatel131129:NV2MO1DOADyUzmnQ@cluster0.iurim.mongodb.net/coursera-app");
+    app.listen(3000);
+    console.log("listening on port 3000");
+
+}
+
+main();
+
+
+
