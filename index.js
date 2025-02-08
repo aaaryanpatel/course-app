26e31d5c55a99e71fb1ec3a0a5a87c3ee85cc641
n <n>
2025-02-07 18:07:08 -0600

    Used dotenv for environment variables

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express');
 const mongoose = require('mongoose');
 
@@ -13,7 +15,7 @@ app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/course", courseRouter);
 
 async function main() {
-    await mongoose.connect("");
+    await mongoose.connect("process.env.MONGO_URL");
     app.listen(3000);
     console.log("listening on port 3000");
 
